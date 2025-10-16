65718b5ae3101e70420a132b52ddb8c76e6ae23d
Lazar Vukasinovic <laki240701@gmail.com>
2025-10-16 14:14:38 +0200

    Define Product struct, mapping and events

--- contracts/WebShop.sol
+++ contracts/WebShop.sol
@@ -2,5 +2,23 @@
 pragma solidity ^0.8.28;
 
 contract WebShop {
+    address public owner;
+    uint public nextProductId = 1;
 
+    struct Product {
+        uint id;
+        string name;
+        uint price; // u wei
+        uint stock;
+    }
+
+    mapping(uint => Product) public products;
+    mapping(address => uint[]) public buyerPurchases;
+
+    // Events
+    event ProductAdded(uint id, string name, uint price, uint stock);
+    event ProductPurchased(address buyer, uint id, uint quantity, uint totalPrice);
+    event ProductRestocked(uint id, uint quantity);
+    event PriceUpdated(uint id, uint newPrice);
+    event ProductRemoved(uint id);
 }
